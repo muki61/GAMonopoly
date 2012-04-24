9b217e8db33acb0719e71b2d3c5201b4c921e19c
muki61 <kmukhar@uccs.edu>
2012-04-24 17:10:00 -0600

    updates to NumWinsFitnessEvaluator

--- Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/ga/NumWinsFitnessEvaluator.java
+++ Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/ga/NumWinsFitnessEvaluator.java
@@ -2,21 +2,37 @@ package edu.uccs.ecgs.ga;
 
 import java.util.HashMap;
 
+/**
+ * Compute fitness based on the number of wins a player has. For each first
+ * place finish in a game, the player is awarded a score that is the number of
+ * players in a game minus 1 (e.g. if there are 4 players in a game, the score
+ * for a win is 3). The player gets 0 points for any other game result (second
+ * through last place).
+ */
 public class NumWinsFitnessEvaluator extends AbstractFitnessEvaluator {
 
   private HashMap<AbstractPlayer, Integer> scores = new HashMap<AbstractPlayer, Integer>();
+  private static final int FIRST_PLACE = 1;
+  private static final int MAX_SCORE = Main.numPlayers - 1;
 
   @Override
   public void evaluate(AbstractPlayer player) {
-    Integer score = scores.get(player);
-    Integer newScore = new Integer(player.getFinishOrder());
+    // Get the current fitness
+    Integer fitness = scores.get(player);
 
-    if (score != null) {
-      newScore = newScore.intValue() + score.intValue();
+    // Compute the score for the most recent game
+    int gameScore = player.getFinishOrder() == FIRST_PLACE ? MAX_SCORE : 0;
+
+    // Add gameScore to fitness
+    if (fitness != null) {
+      fitness = gameScore + fitness;
+    } else {
+      fitness = gameScore;
     }
 
-    scores.put(player, newScore);
-    player.setFitness(newScore.intValue());
+    // Store the new fitness value
+    scores.put(player, fitness);
+    player.setFitness(fitness);
   }
 
 }
