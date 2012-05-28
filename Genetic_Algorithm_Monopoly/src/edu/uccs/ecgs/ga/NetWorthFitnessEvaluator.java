76a8a418b7c0cbb11a8c45b2df7ea337ed942329
muki61 <kmukhar@uccs.edu>
2012-05-28 12:47:27 -0600

    update

--- Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/ga/NetWorthFitnessEvaluator.java
+++ Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/ga/NetWorthFitnessEvaluator.java
@@ -7,7 +7,7 @@ public class NetWorthFitnessEvaluator extends AbstractFitnessEvaluator {
   @Override
   public void evaluate(AbstractPlayer player) {
     // Compute the score for the most recent game
-    int gameScore = player.getTotalWorth() / 100;
+    int gameScore = player.getTotalWorth();
 
     // Store the new fitness value
     player.addToFitness(gameScore);
